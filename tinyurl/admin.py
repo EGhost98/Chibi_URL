0f47ba16ac565e5c383194f6d2ee1681ef3daf91
EGhost98 <adityachauhan8901@gmail.com>
2023-06-19 11:50:17 +0530

    Enhanced Login/Register Pages

--- tinyurl/admin.py
+++ tinyurl/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Shortener
 # Register your models here.
-admin.site.register(Shortener)
+admin.site.site_header = "URL_Shortener Admininstration"
+admin.site.site_title = "URL_Shortener"
+admin.site.index_title = "Manage URL Shortener"
+
+class TinyAdmin(admin.ModelAdmin):
+    
+    list_display = ('created','long_url','short_url',)
+    search_fields = ('long_url',)
+
+
+admin.site.register(Shortener,TinyAdmin)
