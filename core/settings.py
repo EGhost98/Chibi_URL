009cbfd6e4d4e24e141cfe14d9117f1b02153347
Aditya Raj Singh Chauhan <76267623+EGhost98@users.noreply.github.com>
2023-06-21 22:13:41 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -137,6 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
 CSRF_COOKIE_SECURE = True
 
 CSRF_TRUSTED_ORIGINS = ['https://testchibiurl.azurewebsites.net']
